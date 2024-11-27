c786877b57ca11c59f0a189b4d8f91dc2a08b46e
kyle.zheng <kyle.zheng@robosense.cn>
2024-11-27 15:16:32 +0800

    feat: setting redis config and redis service

---
+++ redisPractice/src/main/java/com/example/Service/RedisService.java
@@ -0,0 +1,36 @@
+package com.example.Service;
+
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author kyle.zheng
+ * @date 2024/11/27
+ */
+@Service
+public class RedisService {
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+    // 数据存储
+    public void save(String key,Object value, long timeOut){
+        redisTemplate.opsForValue().set(key,value,timeOut, TimeUnit.SECONDS);
+    }
+    // 数据读取
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    // 数据修改
+    public void update(String key, Object newValue) {
+        redisTemplate.opsForValue().set(key, newValue);
+    }
+
+    // 数据删除
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+}
--- redisPractice/src/main/java/com/example/redisPractice/RedisPracticeApplication.java
+++ redisPractice/src/main/java/com/example/redisPractice/RedisPracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "Controller")
+@ComponentScan(basePackages = "com.example.Controller")
 public class RedisPracticeApplication {
 
 	public static void main(String[] args) {
