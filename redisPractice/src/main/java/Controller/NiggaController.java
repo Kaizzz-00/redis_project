6c63c278cfa3a012bd8a6eb9afca4e60dfd75a5f
kyle.zheng <kyle.zheng@robosense.cn>
2024-11-27 14:35:19 +0800

    feat: adding nigga controller and component scan for starter

---
+++ redisPractice/src/main/java/Controller/NiggaController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kyle.zheng
+ * @date 2024/11/27
+ */
+@RestController
+public class NiggaController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
--- redisPractice/src/main/java/com/example/redisPractice/RedisPracticeApplication.java
+++ redisPractice/src/main/java/com/example/redisPractice/RedisPracticeApplication.java
@@ -2,8 +2,10 @@ package com.example.redisPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "Controller")
 public class RedisPracticeApplication {
 
 	public static void main(String[] args) {
