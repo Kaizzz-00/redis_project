b7c3b6ec68fa2fcb9cdb550aa92915b09148330b
kyle.zheng <kyle.zheng@robosense.cn>
2024-12-02 14:41:24 +0800

    feat: Add comment on Dependency Injection and Bean initialization

--- redisPractice/src/main/java/com/example/Util/RedisLockUtil.java
+++ redisPractice/src/main/java/com/example/Util/RedisLockUtil.java
@@ -13,6 +13,19 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class RedisLockUtil{
     private final RedissonClient redissonClient;
+    /**
+     * The RedissonClient is a Spring-managed bean that needs to be instantiated and injected into this class
+     * during the Spring lifecycle. Therefore, the methods that depend on it must be instance methods (i.e.,
+     * non-static), because Spring cannot inject dependencies into static methods.
+     *
+     * If this utility class does not depend on RedissonClient, all methods can be made static, and the class
+     * could function without relying on Spring's dependency injection.
+     *
+     * In summary, if your utility class needs a non-static instance field (like redissonClient),
+     * the methods that access this field must be non-static as well, in order to allow Spring to inject
+     * the required bean into the instance.
+     */
+
 
     public RedisLockUtil(RedissonClient redissonClient){
         this.redissonClient = redissonClient;
